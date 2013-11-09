73f66511f99abb7b242862083af040cc808a3f68
Tom Sun <sunjie310110@gmail.com>
2013-11-09 18:32:13 +0800

    add template

--- freeapi/freeapi/kuaidi/views.py
+++ freeapi/freeapi/kuaidi/views.py
@@ -4,12 +4,20 @@ import random
 
 import urllib
 from django.http import HttpResponse
+from django.template import loader, Context
+
 
 def index(request):
     """
         测试首页
     """
-    return HttpResponse("Hello, world. You're at the poll index.")
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    t = loader.get_template('kuaidi/index.html')
+    c = Context({
+        'app': 'My app',
+        'message': 'I am the second view.'
+    })
+    return HttpResponse(t.render(c))
 
 def search(request):
     try:
@@ -23,7 +31,7 @@ def searchExpress(expressNo,company='yto'):
     if company=='yto':
         return searchYto(expressNo)
     else:
-        return u"暂不支持公司查询"
+        return u"暂不支持此公司查询"
 
 def searchYto(expressNo):
     rnd = random.randint(0,1)
