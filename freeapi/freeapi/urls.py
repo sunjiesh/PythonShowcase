a2fba3990c355152ec1bf3828156c8ae6e1adfa3
tomsun <sunjie310110@gmail.com>
2013-12-07 20:29:49 +0800

    add error page

--- freeapi/freeapi/urls.py
+++ freeapi/freeapi/urls.py
@@ -10,5 +10,9 @@ urlpatterns = patterns('',
     url(r'^kuaidi', include('freeapi.kuaidi.urls')),
     url(r'^encrypt', include('freeapi.encrypt.urls')),
     url(r'^compare', include('freeapi.compare.urls')),
-    url(r'^$', views.index, name='index'),
+    url(r'^/?$', views.index, name='index'),
 )
+handler500 = 'freeapi.views.custom_500_view'
+handler404 = 'freeapi.views.custom_404_view'
+handler403 = 'freeapi.views.custom_403_view'
+handler400 = 'freeapi.views.custom_400_view'
--- freeapi/freeapi/views.py
+++ freeapi/freeapi/views.py
@@ -15,3 +15,51 @@ def index(request):
         'message': 'I am the second view.'
     })
     return HttpResponse(t.render(c))
+
+def custom_500_view(request):
+    """
+        错误页面
+    """
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    t = loader.get_template('error/500.html')
+    c = Context({
+        'app': 'My app',
+        'message': 'I am the second view.'
+    })
+    return HttpResponse(t.render(c))
+
+def custom_404_view(request):
+    """
+        错误页面
+    """
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    t = loader.get_template('error/404.html')
+    c = Context({
+        'app': 'My app',
+        'message': 'I am the second view.'
+    })
+    return HttpResponse(t.render(c))
+    
+def custom_403view(request):
+    """
+        错误页面
+    """
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    t = loader.get_template('error/403.html')
+    c = Context({
+        'app': 'My app',
+        'message': 'I am the second view.'
+    })
+    return HttpResponse(t.render(c))
+    
+def custom_400_view(request):
+    """
+        错误页面
+    """
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    t = loader.get_template('error/400.html')
+    c = Context({
+        'app': 'My app',
+        'message': 'I am the second view.'
+    })
+    return HttpResponse(t.render(c))
